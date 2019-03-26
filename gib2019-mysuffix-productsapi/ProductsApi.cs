7685465998bb4d0c788f00499d0452f89d262741
Johan Hedberg <johan@jeh.se>
2019-03-26 19:42:02 +0100

    Api now has connection to CosmosDb

--- gib2019-mysuffix-productsapi/ProductsApi.cs
+++ gib2019-mysuffix-productsapi/ProductsApi.cs
@@ -7,6 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents;
+using System.Collections.Generic;
 
 namespace gib2019_mysuffix_productsapi
 {
@@ -21,25 +24,66 @@ namespace gib2019_mysuffix_productsapi
     
     public static class ProductsApi
     {
-        static Product[] sampleProducts = new Product[] { new Product { id = "1", productNumber = 1, category = "bikes", productName = "speeder", description = "pretty" } };
+        private static string EndpointUrl = System.Environment.GetEnvironmentVariable("EndpointUrl");
+        private static string PrimaryKey = System.Environment.GetEnvironmentVariable("PrimaryKey"); 
+        private static string DatabaseName = System.Environment.GetEnvironmentVariable("DatabaseName");
+        private static string CollectionName = System.Environment.GetEnvironmentVariable("CollectionName");
+
+        private static DocumentClient client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
 
         [FunctionName("GetProducts")]
         public static async Task<Product[]> GetAll(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "products")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "products")]
+            HttpRequest req,
             ILogger log)
         {
             log.LogInformation("GetProducts function processed a request.");
-            return sampleProducts;
+
+            int count = 0;
+            string continuation = string.Empty;
+            List<Product> products = new List<Product>();
+            do
+            {
+                // Read the feed 10 items at a time until there are no more items to read
+                FeedResponse<dynamic> response = await client.ReadDocumentFeedAsync(
+                    UriFactory.CreateDocumentCollectionUri(DatabaseName, CollectionName),
+                    new FeedOptions
+                    {
+                        MaxItemCount = 10,
+                        RequestContinuation = continuation
+                    });
+
+                // Append the item count
+                count += response.Count;
+
+                // Get the continuation so that we know when to stop.
+                continuation = response.ResponseContinuation;
+
+                // Loop through documents returned, convert to Product and add to list
+                foreach (var document in response)
+                {
+                    products.Add((Product)document);
+                }
+
+            } while (!string.IsNullOrEmpty(continuation));
+
+            log.LogInformation($"Found {count} products.");
+
+            return products.ToArray();
         }
 
         [FunctionName("GetProduct")]
         public static async Task<Product> Get(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "products/{id}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "products/{category}/{id}")] HttpRequest req,
+            string category, 
             string id,
             ILogger log)
         {
             log.LogInformation("GetProduct function processed a request.");
-            return sampleProducts[int.Parse(id)-1];
+            Product product = await client.ReadDocumentAsync<Product>(
+                UriFactory.CreateDocumentUri(DatabaseName, CollectionName, id), 
+                new RequestOptions { PartitionKey = new PartitionKey(category) });
+            return product;
         }
 
         [FunctionName("CreateProduct")]
@@ -48,21 +92,24 @@ namespace gib2019_mysuffix_productsapi
             ILogger log)
         {
             log.LogInformation("CreateProduct function processed a request.");
-
-            
+            await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(DatabaseName, CollectionName), product);
             return new CreatedResult($"products/{product.id}", product); 
         }
 
         [FunctionName("DeleteProduct")]
         public static async Task<OkResult> Delete(
-            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "products/{id}")] Product product,
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "products/{category}/{id}")] HttpRequest req,
+            string category,
             string id,
             ILogger log)
         {
             log.LogInformation("CreateProduct function processed a request.");
+            Document doc = await client.ReadDocumentAsync(
+                UriFactory.CreateDocumentUri(DatabaseName, CollectionName, id),
+                new RequestOptions { PartitionKey = new PartitionKey(category) });
+            await client.DeleteDocumentAsync(doc.SelfLink,
+                new RequestOptions { PartitionKey = new PartitionKey(category) });
             return new OkResult();
         }
-
-
     }
 }
